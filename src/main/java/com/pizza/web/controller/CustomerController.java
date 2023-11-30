8918d4c4ec6b5f2e19b790cb509b5430c2a79a2e
diegoinost02 <diegoinostroza00@gmail.com>
2023-11-30 15:28:30 -0300

    Initial commit

---
+++ src/main/java/com/pizza/web/controller/CustomerController.java
@@ -0,0 +1,42 @@
+package com.pizza.web.controller;
+
+import com.pizza.persistence.entity.CustomerEntity;
+import com.pizza.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerEntity>> getAll(){
+        try {
+            return ResponseEntity.ok(this.customerService.getAll());
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/phone/{phone}")
+    public ResponseEntity<CustomerEntity> getByPhone(@PathVariable String phone){
+        try {
+            return ResponseEntity.ok(this.customerService.findByPhone(phone));
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
